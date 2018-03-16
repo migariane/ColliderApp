0e5ede3038cf696f6a7b59db7db37b09df97c5e9
migariane <watzilei@hotmail.com>
2018-03-16 14:31:36 +0100

    update.16.03.18

--- app.R
+++ app.R
@@ -4,11 +4,12 @@ library(ggplot2)
 library(readr) # For write_csv
 library(dplyr)
 library(visreg) # Model visualization
+
 generateData <- function(beta1,beta2,n){
     age <- rnorm(n, 65, 5)
     sodium  <- age/15 + rnorm(n) 
-    sbp <- 10*sodium + 1.25*age + rnorm(n)
-    proteinurie  <-  beta1*sbp + beta2*age - 0.9*sodium + rnorm(n)
+    sbp <- 10*sodium + 1.25 * age + rnorm(n)
+    proteinurie  <-  beta1 * sbp + beta2*age - 0.9 * sodium + rnorm(n)
     data.frame(sbp,age,sodium,proteinurie)
 }
 ui <- fluidPage(theme=shinytheme("cosmo"),
@@ -16,10 +17,10 @@ ui <- fluidPage(theme=shinytheme("cosmo"),
   sidebarLayout(
     sidebarPanel(width = 3,
 
-      #Inicialización MathJax para poder incluir fórmulas en LaTeX
+      # Inicialización MathJax para poder incluir fórmulas en LaTeX
       withMathJax(),
 
-      #Model Selection
+      # Model Selection
       h3("Model selection:"),
       checkboxInput(inputId = "modelA", 
                     label = div(h6(withMathJax("$$\\text{SBP}=\\beta_{0}+\\beta_{1}\\text{AGE}$$")),style="margin-top:-10px"),
@@ -36,25 +37,25 @@ ui <- fluidPage(theme=shinytheme("cosmo"),
                     value=TRUE
                     ),
       
-      hr(),
+      hr(), 
       
-      #Slider coeficientes
-      h5(withMathJax("Collider Model: $$\\text{PRO}=\\beta_{0}+\\beta_{1}\\text{AGE}+\\beta_{2}\\text{SBP}+\\beta_{3}\\text{SOD}$$")),
+      # Slider coeficientes
+      h5(withMathJax("Collider Model: $$\\text{PRO}=\\alpha_{0}+\\alpha_{1}\\text{AGE}+\\alpha_{2}\\text{SBP}+\\alpha_{3}\\text{SOD}$$")),
       h6("Move the input slider to visualize the collider"),
       
       sliderInput(inputId = "beta1", 
-                  label = h5(withMathJax("$$\\beta_1\\text{(Effect of SBP on PRO)}$$")),
-                  min=-2,
-                  max=2,
+                  label = h5(withMathJax("$$\\alpha_1\\text{(Effect of SBP on PRO)}$$")),
+                  min=0.5,
+                  max=5,
                   step=0.05,
                   value=1.2
-                  #animate = animationOptions(interval = 10, loop = FALSE)
+                  # animate = animationOptions(interval = 10, loop = FALSE)
                   ),
      
       sliderInput(inputId = "beta2", 
-                  label = h5(withMathJax("$$\\beta_2\\text{(Effect of AGE on PRO)} $$")),
-                  min=-2,
-                  max=2,
+                  label = h5(withMathJax("$$\\alpha_2\\text{(Effect of AGE on PRO)} $$")),
+                  min=0.5,
+                  max=5,
                   step=0.05,
                   value=1.8
                   #animate = animationOptions(interval = 10, loop = FALSE)
@@ -68,40 +69,40 @@ ui <- fluidPage(theme=shinytheme("cosmo"),
                "SBP = Systolic Blood Pressure (mmHg)")
                ),
     
-    # Outputs
+    # Outputs: panel tabs
     mainPanel(
       tabsetPanel(
         
-        #Tab 0: Welcome
+        # Tab 0: Welcome
         tabPanel("Welcome",br(),br(),
                  div(img(src="logo.png",width="450px"),style="text-align:center;")
                  ),
         
-        #Tab 1: DAG
+        # Tab 1: DAG
         tabPanel("DAGs",
                  h2("Directed Acyclic Graphs"),br(),
                  
-                 #No model
-                 conditionalPanel(condition="input.modelA==false && input.modelB==false &&input.modelC==false ",
+                 # No model
+                 conditionalPanel(condition="input.modelA==false && input.modelB==false &&input.modelC==false",
                                   h3(tags$b("Please select a model")),
                                   hr()
                                   ),
 
-                 #Model A
+                 # Model A
                  conditionalPanel(condition="input.modelA==true",
                                   h4(withMathJax("$$\\text{Model 1, SBP} = \\beta_{0} + \\beta_{1} \\text{ AGE}$$")),
                                   div(img(src="dagA.png",width="600px"),style="text-align:center;"),
                                   hr()
                                   ),
 
-                 #Model B
+                 # Model B
                  conditionalPanel(condition="input.modelB==true",
                                   h4(withMathJax("$$\\text{Model 2, SBP} = \\beta_{0} + \\beta_{1} \\text{ AGE} + \\beta_{2} \\text{SOD}$$")),
                                   div(img(src="dagB.png",width="600px"),style="text-align:center;"),
                                   hr()
                                   ),
                
-                 #Model C
+                 # Model C
                  conditionalPanel(condition="input.modelC==true",
                                   h4(withMathJax("$$\\text{Model 3, SBP} = \\beta_{0} + \\beta_{1}  \\text{ AGE} + \\beta_{2} \\text{SOD} + \\beta_{3} \\text{PRO}$$")),
                                   div(img(src="dagC.png",width="600px"),style="text-align:center;"),
@@ -109,16 +110,16 @@ ui <- fluidPage(theme=shinytheme("cosmo"),
                                   )
                  ),
                  
-        #Tab 2: Graphs
+        # Tab 2: Graphs
         tabPanel("Collider Visualization",
                  h3("Effect of Age on SPB for different models' specifications: collider m3"),br(),
-                 #No model
-                 conditionalPanel(condition="input.modelA==false && input.modelB==false &&input.modelC==false ",
+                 # No model
+                 conditionalPanel(condition="input.modelA==false && input.modelB==false && input.modelC==false",
                                   h3(tags$b("Please select a model")),
                                   hr()
                                   ),
 
-                 #Models: formula + plot + a
+                 # Models: formula + plot + beta1
                  fluidRow(column(4,h4(uiOutput("formulaA"))),
                           column(4,h4(uiOutput("formulaB"))),
                           column(4,h4(uiOutput("formulaC")))
@@ -141,7 +142,7 @@ ui <- fluidPage(theme=shinytheme("cosmo"),
         tabPanel("Data generation",
                  h2("Code for data generation"), br(),
                  
-                 #Code
+                 # Code
                  tags$p("generateData <- function(beta1,beta2,n){",br(),
                         "age <- rnorm(n, 65, 5)",br(),
                         "sodium  <- age/15 + rnorm(n)",br(), 
@@ -169,7 +170,7 @@ ui <- fluidPage(theme=shinytheme("cosmo"),
                  hr()
                  ),
         
-        #Tab 4: Article
+        # Tab 4: Article
         tabPanel("Article",br(),br(),
                  uiOutput("article"))
         ,
@@ -240,40 +241,40 @@ output$formulaC<-renderUI({
 ObsData <- reactive({set.seed(777)
                      generateData(input$beta1,input$beta2,n=1000)})
 
-#Head from simulated data 
+# Head from simulated data 
 output$table_generateData<-renderTable(head(ObsData()))
 
-#Linear models fits + graphs
+# Linear models fits + graphs
 fit1 <- reactive({lm(sbp ~ age, data=ObsData())})
 fit2 <- reactive({lm(sbp ~ age + sodium, data=ObsData())})
 fit3 <- reactive({lm(sbp ~ age + proteinurie + sodium, data=ObsData())})
 
-grafico1<-reactive({visreg(fit1(), points=list(cex=1.5, pch=1), jitter=10,bty="n")})
-grafico2<-reactive({visreg(fit2(), points=list(cex=1.5, pch=1), jitter=10,bty="n")})
-grafico3<-reactive({visreg(fit3(), points=list(cex=1.5, pch=1), jitter=10,bty="n")})
+grafico1 <- reactive({visreg(fit1(), points=list(cex=1.5, pch=1), jitter=10,bty="n")})
+grafico2 <- reactive({visreg(fit2(), points=list(cex=1.5, pch=1), jitter=10,bty="n")})
+grafico3 <- reactive({visreg(fit3(), points=list(cex=1.5, pch=1), jitter=10,bty="n")})
 
-#Figures
+# Figures
 output$gA1<-renderPlot({
   if(input$modelA==TRUE) plot(grafico1(),gg=TRUE,ylab="SBP (mmHg)",xlab="Age (years)",
-                              points=list(size=2, pch=1,alpha=0.4,col="snow3"), line=list(col="blue",size=1.3))+ theme_classic()
+                              points=list(size=2, pch=1,alpha=0.4,col="snow3"), line=list(col="blue",size=1.3)) + theme_classic()
 })
 
 output$gB1<-renderPlot({
   if(input$modelB==TRUE) plot(grafico2()[[1]],gg=TRUE,ylab="SBP (mmHg)",xlab="Age (years)",
-                              points=list(size=2, pch=1,alpha=0.4,col="snow3"), line=list(col="blue",size=1.3))+ theme_classic()
+                              points=list(size=2, pch=1,alpha=0.4,col="snow3"), line=list(col="blue",size=1.3)) + theme_classic()
 })
 
-#Positive slope -> blue; negative -> red
+# Positive slope -> blue; negative -> red
 output$gC1<-renderPlot({
         if(input$modelC==TRUE)
           if(fit3()$coefficients["age"]>0) plot(grafico3()[[1]],gg=TRUE,ylab="SBP (mmHg)",xlab="Age (years)",
-                                                points=list(size=2, pch=1,alpha=0.4,col="snow3"), line=list(col="blue",size=1.3))+ theme_classic()
-  else 
+                                                points=list(size=2, pch=1,alpha=0.4,col="snow3"), line=list(col="blue",size=1.3)) + theme_classic()
+          else 
             plot(grafico3()[[1]],gg=TRUE,ylab="SBP (mmHg)",xlab="Age (years)",
-                 points=list(size=2, pch=1,alpha=0.4,col="snow3"), line=list(col="red",size=1.3))+ theme_classic()
+                 points=list(size=2, pch=1,alpha=0.4,col="snow3"), line=list(col="red",size=1.3)) + theme_classic()
 })
 
-#Coefficients
+# Coefficients
 output$coefA<-renderUI({
   if(input$modelA==TRUE)  withMathJax(sprintf("$$ \\beta_{1} = %.03f$$", fit1()$coefficients["age"]))
 })
@@ -286,23 +287,23 @@ output$coefC<-renderUI({
   if(input$modelC==TRUE)  withMathJax(sprintf("$$ \\beta_{1} = %.03f$$", fit3()$coefficients["age"]))
 })
 
-#Figures from different models 
-output$data.generation.plot1<-renderPlot({
+# Figures for model 3 in data generation tab
+output$data.generation.plot1 <- renderPlot({
   plot(grafico3()[[1]],gg=TRUE,ylab="SBP (mmHg)",xlab="Age (years)",
        line=list(col="darkmagenta",size=1.3),points=list(size=2, pch=1,alpha=0.4,col="snow3"))+ theme_classic()
 })
 
-output$data.generation.plot2<-renderPlot({
+output$data.generation.plot2 <- renderPlot({
   plot(grafico3()[[2]],gg=TRUE,ylab="SBP (mmHg)",xlab="Proteinurie (mg)",
-       points=list(size=2, pch=1,alpha=0.4,col="snow3"), line=list(col="darkmagenta",size=1.3))+ theme_classic()
+       points=list(size=2, pch = 1,alpha=0.4,col="snow3"), line=list(col="darkmagenta",size=1.3))+ theme_classic()
 })
 
-output$data.generation.plot3<-renderPlot({
+output$data.generation.plot3 <- renderPlot({
   plot(grafico3()[[3]],gg=TRUE,ylab="SBP (mmHg)",xlab="Sodium (g)",
-       points=list(size=2, pch=1,alpha=0.4,col="snow3"), line=list(col="darkmagenta",size=1.3))+ theme_classic()
+       points=list(size=2, pch=1,alpha = 0.4,col="snow3"), line=list(col="darkmagenta",size = 1.3))+ theme_classic()
 })
 
-#Download data
+# Download data
 output$download_data <- downloadHandler(
           filename = function() {
             paste0("data.csv")
@@ -312,12 +313,12 @@ output$download_data <- downloadHandler(
           }
     )
 
-#Article
+# Article
 output$article <- renderUI({
     tags$iframe(style="height:700px; width:100%", src="Paper_Collider.pdf")
 })
 
 }
 
-#Building the shiny application
+# Building the shiny application
 shinyApp(ui = ui, server = server)
