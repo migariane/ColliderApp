70b67747f7711662213e4a9a416a3f26ed213f06
migariane <watzilei@hotmail.com>
2018-08-27 11:06:45 +0200

    Update.27.8.18

--- code_boxes_article.R
+++ code_boxes_article.R
@@ -1,3 +1,25 @@
+###############################################################################################
+# Educational Note: Paradoxical Collider Effect in the Analysis of Non-Communicable Disease 
+# Epidemiological Data: a reproducible illustration and web application
+###############################################################################################
+# Copyright (c) 2018  <Miguel Angel Luque-Fernandez>
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+##############################################################################################
+
 # Box 1 --------------------
 library(visreg) # to visualize regression output
 library(ggplot2) # to visualize regression output
